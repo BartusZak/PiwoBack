602301d19956cf4dc3bbc0b424cf8a0efdbaa4f6
Adam Sobiecki <136228@student.uwm.edu.pl>
2018-04-10 15:32:27 +0200

    Utworzenie solucji

---
+++ PiwoBack.Data/Models/BrewingGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PiwoBack.Data.Models
+{
+    public class BrewingGroup : Entity
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public DateTime CreateDate { get; set; }
+        public ICollection<Brewery> Breweries { get; set; }
+
+    }
+}
---
+++ PiwoBack.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using PiwoBack.Data.Models;
+
+namespace PiwoBack.Repository.Interfaces
+{
+    public interface IRepository<T> where T : Entity
+    {
+        IEnumerable<T> GetAll(params Expression<Func<T, object>>[] includes);
+        IEnumerable<T> GetAllBy(Expression<Func<T, bool>> getBy, params Expression<Func<T, object>>[] includes);
+        T GetBy(Expression<Func<T, bool>> getBy, params Expression<Func<T, object>>[] includes);
+        bool Exist(Expression<Func<T, bool>> expression);
+        int Insert(T entity);
+        void Update(T entity);
+        void Delete(Expression<Func<T, bool>> expression);
+    }
+}
